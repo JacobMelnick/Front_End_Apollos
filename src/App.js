95ab5c5549cc570145b95c7b67b9f15a582bbb55
Jacob Melnick <Jake.r.melnick@gmail.com>
2022-03-16 16:30:27 -0500

    connected to backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
+import { qgl, useMutation, useQuery } from "@apollo/client";
+import { useEffect } from "react";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello CodeSandbox</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://0eqhev.sse.codesandbox.io/",
+  cache: new InMemoryCache()
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
   rootElement
 );
