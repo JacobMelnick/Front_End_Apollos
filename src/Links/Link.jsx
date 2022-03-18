5238385a08f179450bf3b187487fba7eba27a595
Jacob Melnick <Jake.r.melnick@gmail.com>
2022-03-18 13:12:22 -0500

    displaying data from backend to links

--- src/Links/Link.jsx
+++ src/Links/Link.jsx
@@ -1,7 +1,20 @@
 import react from "react";
+import { Grid } from "@mui/material";
 
-const Link = () => {
-  return <div>hello</div>;
+const Link = ({ linkData }) => {
+  return (
+    <Grid>
+      {linkData.length > 0 &&
+        linkData.map((link) => {
+          // console.log(link.url);
+          return (
+            <Grid item xs={4} lg={12} md={9}>
+              {link.url}
+            </Grid>
+          );
+        })}
+    </Grid>
+  );
 };
 
 export default Link;
--- src/Links/Links.jsx
+++ src/Links/Links.jsx
@@ -1,7 +1,20 @@
-import react from "react";
+import react, { useState, useEffect } from "react";
 import { Grid } from "@mui/material";
 import Link from "../Links/Link";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_LINKS } from "../graphql/query";
+
 const Links = () => {
+  const { loading, error, data } = useQuery(GET_ALL_LINKS);
+  const [linkData, setLinkData] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+
+      setLinkData(data.list);
+    }
+  }, [data]);
   return (
     <Grid
       style={{ padding: 10, margin: 10 }}
@@ -10,7 +23,7 @@ const Links = () => {
       alignItems="center"
       direction="column"
     >
-      <Link />
+      <Link linkData={linkData} />
     </Grid>
   );
 };
