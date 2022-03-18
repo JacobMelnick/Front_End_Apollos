cf5a5a70ac66741dc802cd7f9df9f652be74f230
Jacob Melnick <Jake.r.melnick@gmail.com>
2022-03-18 12:21:53 -0500

    created mutations and queries front end

--- src/graphql/mutation.js
+++ src/graphql/mutation.js
@@ -1,1 +1,9 @@
 import { gql } from "@apollo/client";
+
+export const CREATE_URL = gql`
+  mutation createLink($id: String!, $url: String!) {
+    createLink(id: $id, url: $url) {
+      id
+    }
+  }
+`;
--- src/graphql/query.js
+++ src/graphql/query.js
@@ -1,1 +1,10 @@
 import { gql } from "@apollo/client";
+
+export const GET_ALL_LINKS = gql`
+  query getAllLink {
+    list {
+      id
+      url
+    }
+  }
+`;
